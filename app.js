3125bbe3362efbfd2565b21bbae2778db5b5e412
Temitope Emmanuel Owolabi <owolabitemmythope007@gmail.com>
2022-10-16 15:30:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const myMap = L.map('map').setView([7.594377534, 5.299280057], 13);  
+const myMap = L.map('map').setView([7.594377534, 5.299280057], 14);  
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const attribution =
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>&mdash;<a>GoldenHeart_007</a>'
