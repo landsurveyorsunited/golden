64a155e29966a6aa4275ddfa660cb72034bbf4ca
Temitope Emmanuel Owolabi <owolabitemmythope007@gmail.com>
2023-01-05 16:00:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -139,6 +139,7 @@ L.control.betterscale({
 //Users Adding Coordinates
 L.control.coordinates({
 	position:"topleft", //optional default "topleft"
+        collapsible:"false"
 	decimals:4, //optional default 4
 	decimalSeperator:".", //optional default "."
 	labelTemplateLat:"Latitude: {y}", //optional default "Lat: {y}"
